2eef5500f3eb392c3b0597a0b2e1a60179d52145
dllo <xiaosha3385@126.com>
2016-07-15 15:26:01 +0800

    在new 里extends Activity 连嘉凡

--- app/src/main/java/com/xdx/dllo/didi/New.java
+++ app/src/main/java/com/xdx/dllo/didi/New.java
@@ -1,7 +1,15 @@
 package com.xdx.dllo.didi;
 
+import android.app.Activity;
+import android.os.Bundle;
+
 /**
  * Created by dllo on 16/7/15.
  */
-public class New {
+public class New extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
 }
